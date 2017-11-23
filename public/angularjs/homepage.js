57a26f168cb842e549f466d7c6a14f82c7189a9b
tusharkailash <tusharkailash29@gmail.com>
2017-11-22 18:33:57 -0800

    Added flights_search html and css files

--- public/angularjs/homepage.js
+++ public/angularjs/homepage.js
@@ -1,121 +1,148 @@
 
-var search_flight = angular.module('searchflight', []);
-search_flight.controller('searchflight', function($scope, $http) {
+
+var app = angular.module('searchflight', ["ngRoute"]);
+app.config(function($routeProvider)
+{
+    $routeProvider
+        .when("/search", {
+            templateUrl : "flight_search.htm",
+            controller : "searchflight"      //to be edited later
+        })
+
+app.controller('searchflight', function($scope, $http) {
     $scope.unexpected_error = true;
 
 
-    $scope.searchflight = function () {
+        $scope.searchflight = function () {
 
-        if (($scope.oneway) && ($scope.preferred_flights != null)) {
-            var data1_p = {
-                source: $scope.source,
-                destination: $scope.destination,
-                travel_date: Sscope.travel_date,
-                preferred_flights: $scope.preferred_flights
-            };
-            $http({
-                method: 'POST',       //Using http method POST
-                url: '/oneway_preferred',
-                headers: {'Content-Type': 'application/json'},
-                data: data1_p
-            })
-                .success(function (data) {         //Check response and redirect user to appropriate page
-                    if (data.statusCode == 401) {
-                        console.log('error finding flight');
-                        window.alert('Invalid entry');    //If user is not validated then alert user
-
-                        $scope.unexpected_error = true;
-                    } else if (data.statusCode === 200) {     //If user validated successfully redirect to homepage
-                        window.location.assign('/confirmation');
-                    }
+            if (($scope.oneway) && ($scope.preferred_flights != null)) {
+                var data1_p = {
+                    source: $scope.source,
+                    destination: $scope.destination,
+                    travel_date: Sscope.travel_date,
+                    preferred_flights: $scope.preferred_flights
+                };
+                $http({
+                    method: 'POST',       //Using http method POST
+                    url: '/oneway_preferred',
+                    headers: {'Content-Type': 'application/json'},
+                    data: data1_p
                 })
-                .error(function (error) {
-                    $scope.unexpected_error = false;
-                });
-        }
-        else if (($scope.oneway) && ($scope.preferred_flights == null)) {
-            var data1_np = {
-                source: $scope.source,
-                destination: $scope.destination,
-                travel_date: Sscope.travel_date,
+                    .success(function (response) {         //Check response and redirect user to appropriate page
+                        if (data.statusCode == 401) {
+                            console.log('error finding flight');
+                            window.alert('Invalid entry');    //If user is not validated then alert user
 
-            };
-            $http({
-                method: 'POST',       //Using http method POST
-                url: '/search_oneway',
-                headers: {'Content-Type': 'application/json'},
-                data: data1_np
-            })
-                .success(function (data) {         //Check response and redirect user to appropriate page
-                    if (data.statusCode == 401) {
-                        console.log('error finding flight');
-                        window.alert('Invalid entry');    //If user is not validated then alert user
+                            $scope.unexpected_error = true;
+                        } else if (data.statusCode === 200) {     //If user validated successfully redirect to homepage
+                           // window.location.assign('/flight_list');  //need to extract data frm response n provide to next page
+                                                                  // get the data from database ($scope.oneway = data.oneway) and provide it
+                                                                  // to the next page.
+                            $scope.source = response.source
+                            $scope.destination = response.destination
+                            $scope.price = response.price
 
-                        $scope.unexpected_error = true;
-                    } else if (data.statusCode === 200) {     //If user validated successfully redirect to homepage
-                        window.location.assign('/confirmation');
-                    }
+
+                        }
+                    })
+                    .error(function (error) {
+                        $scope.unexpected_error = false;
+                    });
+
+
+            }
+            else if (($scope.oneway) && ($scope.preferred_flights == null)) {
+                var data1_np = {
+                    source: $scope.source,
+                    destination: $scope.destination,
+                    travel_date: Sscope.travel_date,
+
+                };
+                $http({
+                    method: 'POST',       //Using http method POST
+                    url: '/search_oneway',
+                    headers: {'Content-Type': 'application/json'},
+                    data: data1_np
                 })
-                .error(function (error) {
-                    $scope.unexpected_error = false;
-                });
-        }
+                    .success(function (response) {         //Check response and redirect user to appropriate page
+                        if (data.statusCode == 401) {
+                            console.log('error finding flight');
+                            window.alert('Invalid entry');    //If user is not validated then alert user
 
-        else if (($scope.twoway) && ($scope.preferred_flights != null)) {
-            var data2_p = {
-                source: $scope.source,
-                destination: $scope.destination,
-                travel_date: Sscope.travel_date,
-                return_date: $scope.return_date,
-                preferred_flights: $scope.preferred_flights
-            };
-            $http({
-                method: 'POST',       //Using http method POST
-                url: '/twoway_preferred',
-                headers: {'Content-Type': 'application/json'},
-                data: data2_p
-            })
-                .success(function (data) {         //Check response and redirect user to appropriate page
-                    if (data.statusCode == 401) {
-                        console.log('error finding flight');
-                        window.alert('Invalid entry');    //If user is not validated then alert user
+                            $scope.unexpected_error = true;
+                        } else if (data.statusCode === 200) {     //If user validated successfully redirect to homepage
+                            //window.location.assign('/confirmation');
+                            $scope.source = response.source
+                            $scope.destination = response.destination
+                            $scope.price = response.price
+                        }
+                    })
+                    .error(function (error) {
+                        $scope.unexpected_error = false;
+                    });
+            }
 
-                        $scope.unexpected_error = true;
-                    } else if (data.statusCode === 200) {     //If user validated successfully redirect to homepage
-                        window.location.assign('/confirmation');
-                    }
+            else if (($scope.twoway) && ($scope.preferred_flights != null)) {
+                var data2_p = {
+                    source: $scope.source,
+                    destination: $scope.destination,
+                    travel_date: Sscope.travel_date,
+                    return_date: $scope.return_date,
+                    preferred_flights: $scope.preferred_flights
+                };
+                $http({
+                    method: 'POST',       //Using http method POST
+                    url: '/twoway_preferred',
+                    headers: {'Content-Type': 'application/json'},
+                    data: data2_p
                 })
-                .error(function (error) {
-                    $scope.unexpected_error = false;
-                });
-        }
+                    .success(function (response) {         //Check response and redirect user to appropriate page
+                        if (data.statusCode == 401) {
+                            console.log('error finding flight');
+                            window.alert('Invalid entry');    //If user is not validated then alert user
 
-        else if (($scope.twoway) && ($scope.preferred_flights == null)) {
-            var data2_np = {
-                source: $scope.source,
-                destination: $scope.destination,
-                travel_date: Sscope.travel_date,
-                return_date: $scope.return_da te
-            };
-            $http({
-                method: 'POST',       //Using http method POST
-                url: '/search_twoway',
-                headers: {'Content-Type': 'application/json'},
-                data: data2_np
-            })
-                .success(function (data) {         //Check response and redirect user to appropriate page
-                    if (data.statusCode == 401) {
-                        console.log('error finding flight');
-                        window.alert('Invalid entry');    //If user is not validated then alert user
+                            $scope.unexpected_error = true;
+                        } else if (data.statusCode === 200) {     //If user validated successfully redirect to homepage
+                            //window.location.assign('/confirmation');
+                            $scope.source = response.source
+                            $scope.destination = response.destination
+                            $scope.price = response.price
+                        }
+                    })
+                    .error(function (error) {
+                        $scope.unexpected_error = false;
+                    });
+            }
 
-                        $scope.unexpected_error = true;
-                    } else if (data.statusCode === 200) {     //If user validated successfully redirect to homepage
-                        window.location.assign('/confirmation');
-                    }
+            else if (($scope.twoway) && ($scope.preferred_flights == null)) {
+                var data2_np = {
+                    source: $scope.source,
+                    destination: $scope.destination,
+                    travel_date: Sscope.travel_date,
+                    return_date: $scope.return_da te
+                };
+                $http({
+                    method: 'POST',       //Using http method POST
+                    url: '/search_twoway',
+                    headers: {'Content-Type': 'application/json'},
+                    data: data2_np
                 })
-                .error(function (error) {
-                    $scope.unexpected_error = false;
-                });
-        }
-    };
+                    .success(function (response) {         //Check response and redirect user to appropriate page
+                        if (data.statusCode == 401) {
+                            console.log('error finding flight');
+                            window.alert('Invalid entry');    //If user is not validated then alert user
+
+                            $scope.unexpected_error = true;
+                        } else if (data.statusCode === 200) {     //If user validated successfully redirect to homepage
+                            //window.location.assign('/confirmation');
+                            $scope.source = response.source
+                            $scope.destination = response.destination
+                            $scope.price = response.price
+                        }
+                    })
+                    .error(function (error) {
+                        $scope.unexpected_error = false;
+                    });
+            }
+        };
 });
